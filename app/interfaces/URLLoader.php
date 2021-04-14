0118d9285eb7873fc5ddce98f33210341e6fad4d
amatveev <skyclr@gmail.com>
2021-04-14 13:30:53 +0300

    Readme and renames

--- app/interfaces/URLLoader.php
+++ app/interfaces/URLLoader.php
@@ -6,5 +6,10 @@ namespace App\Interfaces;
 
 interface URLLoader
 {
+    /**
+     * Loads resource by url
+     * @param string $url URL of destination point
+     * @return string Result from point
+     */
     public function loadUrl(string $url): string;
 }
